914555fdc5c85ead96c6034047be48a975b3c299
alexandersimbirtsev <alexandersimbirtcev@gmail.com>
2021-10-25 17:49:49 +0300

    Initial Commit

---
+++ Articles/ArticlesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArticlesApp.swift
+//  Articles
+//
+//  Created by Александр on 25.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct ArticlesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
